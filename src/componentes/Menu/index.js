927a17c6ee305cbafa14eac6015b990f64d17fff
pedroquintao <pedro.h.quintao@hotmail.com>
2023-11-24 22:30:01 -0300

    Continuar pela aula 02 atividade 07: Desafio: utilizando NavLink

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import styles from './Menu.module.css'
 import { Link, useLocation } from 'react-router-dom'
+import MenuLink from './MenuLink'
 
 export default function Menu() {
-const location = useLocation();
-console.log(location)
+
 
   return (
     <header>
         <nav className={styles.navegacao}>
-            <Link className={`${styles.link}${location.pathname === '/' ?
-                                styles.linkDestacado : ''}`} to='/'>
-                Início
-            </Link>
-            <Link className={styles.link} to='/sobremim'>
-                Sobremim
-            </Link>
+            <MenuLink to='/'>
+              Inicio
+            </MenuLink>
+            <MenuLink to='/sobremim'>
+              Sobre mim
+            </MenuLink>
         </nav>
     </header>
   )
